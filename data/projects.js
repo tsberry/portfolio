5c2e81192a9b17394a2045774c94519f36b24b20
unknown <tberry6@gmail.com>
2018-05-02 15:56:47 -0700

    Added new project and image for star wars game; modified a bit of css

--- data/projects.js
+++ data/projects.js
@@ -5,8 +5,10 @@ function Project(name, image, repository) {
 }
 
 var projects = [];
+projects.push(new Project("Jokester", "/assets/images/jokester.png", "https://github.com/tsberry/jokester"));
 projects.push(new Project("Face Yourself", "/assets/images/faceyourself.jpg", "https://github.com/davidlatuno/faceyourself"));
 projects.push(new Project("Friend Finder", "/assets/images/friendfinder.jpg", "https://github.com/tsberry/friendfinder"));
 projects.push(new Project("Bamazon", "/assets/images/bamazon.png", "https://github.com/tsberry/bamazon"));
+projects.push(new Project("Star Wars Game", "/assets/images/starwars.png", "https://github.com/tsberry/week-4-game-hard"));
 
 module.exports = projects;
